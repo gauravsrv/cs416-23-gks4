76ceea522e4e14c4b045176382912984a9a39aec
Gaurav Srivastava <srivastavagrv@gmail.com>
2023-07-29 05:27:30 +0530

    Screen1 handle exact year

--- script.js
+++ script.js
@@ -21,69 +21,77 @@ document.addEventListener("DOMContentLoaded", function () {
         setSelectedButton(slideIndex);
     });
 
-    // Slide 1: Top 10 Airlines for each year between 2015 to 2019
-    d3.csv("Airline_review.csv").then(data => {
-        // Convert Overall_Rating to a number for sorting
-        data.forEach(d => {
-            d.Overall_Rating = +d.Overall_Rating;
+    // Show the first slide and select the first button by default
+    showSlide(1);
+    setSelectedButton(1);
+
+    // Load the CSV data and create the chart in slide 1
+    d3.csv("Airline_review.csv").then(function (data) {
+        const years = [2015, 2016, 2017, 2018, 2019];
+
+        const airlinesByYear = {};
+        years.forEach((year) => {
+            airlinesByYear[year] = data
+                .filter((d) => parseInt(d["Review_year"]) === year)
+                .sort((a, b) => b["Overall_Rating"] - a["Overall_Rating"])
+                .slice(0, 10)
+                .map((d) => ({
+                    Airline: d["Airline Name"],
+                    Rating: parseFloat(d["Overall_Rating"]),
+                }));
         });
 
-        const filteredData = data.filter(d => d.Review_year && d.Review_year.length >= 4);
-
-        const nestedData = d3.group(filteredData, d => d.Review_year.substring(0, 4));
-
-
-        const margin = { top: 20, right: 30, bottom: 60, left: 100 };
+        const margin = { top: 20, right: 20, bottom: 50, left: 80 };
         const width = 800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
 
-        const svg = d3.select("#slide1")
+        const svg = d3.select("#chartContainer")
             .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
+            .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
         const xScale = d3.scaleBand()
-            .domain(nestedData.get("2015").map(d => d.Airline_Name))
+            .domain(years)
             .range([0, width])
-            .padding(0.2);
+            .padding(0.1);
 
         const yScale = d3.scaleLinear()
-            .domain([0, 10])
+            .domain([0, 10]) // Assuming the rating ranges from 0 to 10
             .range([height, 0]);
 
-        const xAxis = d3.axisBottom(xScale);
-        const yAxis = d3.axisLeft(yScale);
-
         svg.append("g")
-            .attr("class", "x-axis")
-            .attr("transform", `translate(0,${height})`)
-            .call(xAxis)
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-65)");
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(xScale));
 
-        svg.append("g")
-            .attr("class", "y-axis")
-            .call(yAxis);
+        svg.append("g").call(d3.axisLeft(yScale));
 
-        // Create bars
-        const bars = svg.selectAll(".bar")
-            .data(nestedData.get("2015"))
+        const barGroup = svg.selectAll(".barGroup")
+            .data(years)
+            .enter()
+            .append("g")
+            .attr("class", "barGroup")
+            .attr("transform", (d) => `translate(${xScale(d)}, 0)`);
+
+        barGroup.selectAll("rect")
+            .data((d) => airlinesByYear[d])
             .enter()
             .append("rect")
-            .attr("class", "bar")
-            .attr("x", d => xScale(d.Airline_Name))
+            .attr("x", 0)
+            .attr("y", (d) => yScale(d.Rating))
             .attr("width", xScale.bandwidth())
-            .attr("y", d => yScale(d.Overall_Rating))
-            .attr("height", d => height - yScale(d.Overall_Rating))
+            .attr("height", (d) => height - yScale(d.Rating))
             .attr("fill", "#007BFF");
-    });
 
-    // Show the first slide and select the first button by default
-    showSlide(1);
-    setSelectedButton(1);
+        barGroup.selectAll("text")
+            .data((d) => airlinesByYear[d])
+            .enter()
+            .append("text")
+            .attr("x", xScale.bandwidth() / 2)
+            .attr("y", (d) => yScale(d.Rating) - 5)
+            .attr("text-anchor", "middle")
+            .text((d) => d.Rating.toFixed(1))
+            .attr("fill", "#fff");
+    });
 });
