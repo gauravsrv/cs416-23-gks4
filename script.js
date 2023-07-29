e403817494ded6b8db4fbdb244870fa6b2d5825e
Gaurav Srivastava <srivastavagrv@gmail.com>
2023-07-29 16:23:40 +0530

    Screen1 legend fix

--- script.js
+++ script.js
@@ -48,7 +48,8 @@ document.addEventListener("DOMContentLoaded", function () {
             .domain(airlinesToPlot)
             .range(d3.schemeCategory10); // Using D3's built-in color scheme
 
-        const margin = { top: 20, right: 20, bottom: 50, left: 80 };
+         const legendWidth = 120;   
+        const margin = { top: 20, right: legendWidth, bottom: 50, left: 80 };
         const width = 800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
         let mouseX = 0;
@@ -113,10 +114,10 @@ document.addEventListener("DOMContentLoaded", function () {
             .attr("class", "tooltip")
             .style("opacity", 0);
 
-        const legendWidth = 120;
+       
         const legend = svg.append("g")
             .attr("class", "legend")
-            .attr("transform", `translate(${width + legendWidth}, 20)`); // Move the legend to the right side
+            .attr("transform", `translate(${width}, 20)`); // Move the legend to the right side
 
 
         const legendItems = legend.selectAll(".legendItem")
