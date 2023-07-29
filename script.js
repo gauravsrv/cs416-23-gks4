48e8ffe82456e7a64169441a4a56e9e6f8f724c9
Gaurav Srivastava <srivastavagrv@gmail.com>
2023-07-29 15:58:18 +0530

    Screen1 add animation

--- script.js
+++ script.js
@@ -51,6 +51,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const margin = { top: 20, right: 20, bottom: 50, left: 80 };
         const width = 800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
+        let mouseX = 0;
+        let mouseY = 0;
 
         const svg = d3.select("#chartContainer")
             .append("svg")
@@ -161,7 +163,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Get the mouse position on the SVG container
         svg.on("mousemove", function () {
-            const [mouseX, mouseY] = d3.mouse(this);
+            [mouseX, mouseY] = d3.mouse(this);
             updateGraph(yearIndex);
         });
     });
